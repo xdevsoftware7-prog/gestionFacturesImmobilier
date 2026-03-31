40a2862b8000a5984c79fcb8d0ef758dbe0a13d8
xdevsoftware7-prog <xdevsoftware7@gmail.com>
2026-03-31 10:36:30 +0100

    Suppression de verifyAuth, parce que l'idee c'est de centrer la logique de auth et de securise les routes, je pense à utiliser APIGATEWAY

--- services/auth-service/service.js
+++ services/auth-service/service.js
@@ -61,31 +61,7 @@ const authorizeService = (serviceRequis,role) => {
 };
 
 
-const verifyAuth = (serviceAutorise) => {
-    return async (req, res, next) => {
-        const token = req.headers['authorization'];
-        if (!token) return res.status(401).json({ message: 'Token manquant' });
 
-        try {
-            // Appel au microservice auth-service
-            const response = await axios.get('http://auth-service:3001/api/auth/verify', {
-                headers: { 'Authorization': token }
-            });
-
-            const user = response.data.user;
-
-            // Vérification : l'utilisateur appartient-il au bon service ? ou s'il s'agit de l'admin
-            if (response.data.valid && (user.service === serviceAutorise || user.role === 'admin')) {
-                req.user = user;
-                next();
-            } else {
-                res.status(403).json({ message: `Accès interdit : réservé au service ${serviceAutorise}` });
-            }
-        } catch (error) {
-            res.status(401).json({ message: 'Session invalide' });
-        }
-    };
-};
 
 app.get('/api/auth/verify', verifyToken, (req, res) => {
     // Si le middleware verifyToken passe, c'est que le token est valide
